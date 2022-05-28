b988a87a0ee91f0c029ace16cf833f22f181cce0
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2022-05-28 16:51:05 +0200

    Improved search form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert("Searching for the new keyword");
+    alert(`Searching for the new ${keyword}`);
   }
 
   function handleKeywordChange(event) {
