2396704a99a62e110856d2d60838bbe107d6492b
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2022-06-07 20:44:25 +0200

    Styled the page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <h1>English Dictionary</h1>
         <main>
           <Dictionary />
         </main>
